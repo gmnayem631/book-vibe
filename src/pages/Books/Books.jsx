6b43867cf6b228e9188cfcd88bbaf42cea85d070
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-04-21 09:59:30 +0600

    added some styles to the cards, made the cards dynamic

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -6,7 +6,7 @@ const Books = ({ data }) => {
     <div>
       <h1 className="text-3xl text-center">This is books</h1>
       <Suspense fallback={<span>Loading....</span>}>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {data.map((singleBook) => (
             <Book key={singleBook.bookId} singleBook={singleBook}></Book>
           ))}
