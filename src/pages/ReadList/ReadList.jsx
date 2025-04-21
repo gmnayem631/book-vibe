b15a2f1c062ceeb7f4f9345813c80dd77af9834b
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-04-21 17:43:56 +0600

    sorted the book list by page number and ratings

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -7,6 +7,7 @@ import Book from "../Book/Book";
 
 const ReadList = () => {
   const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("");
 
   const data = useLoaderData();
   console.log(data);
@@ -20,8 +21,34 @@ const ReadList = () => {
     );
     setReadList(myReadList);
   }, []);
+
+  const handleSort = (type) => {
+    setSort(type);
+    if (type === "pages") {
+      const sortedByPage = [...readList].sort(
+        (a, b) => a.yearOfPublishing - b.yearOfPublishing
+      );
+      setReadList(sortedByPage);
+    }
+    if (type === "ratings") {
+      const sortedByRating = [...readList].sort((a, b) => a.rating - b.rating);
+      setReadList(sortedByRating);
+    }
+  };
+
   return (
     <div>
+      <details className="dropdown">
+        <summary className="btn m-1">Sort by: {sort ? sort : ""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={() => handleSort("pages")}>Pages</a>
+          </li>
+          <li>
+            <a onClick={() => handleSort("ratings")}>Ratings</a>
+          </li>
+        </ul>
+      </details>
       <Tabs>
         <TabList>
           <Tab>Read Book List</Tab>
