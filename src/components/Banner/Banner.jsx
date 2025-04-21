9511dd4d6ba5655b6c33d74a86c4616afe3ca14d
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-04-21 07:37:45 +0600

    styled the banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import bookImage from "../../assets/books.jpg";
+import bookImage from "../../assets/banner-bg.png";
 
 const Banner = () => {
   return (
-    <div className="flex justify-between items-center p-16 w-full mx-auto">
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo,
-          officiis.
+    <div className="flex justify-between items-center p-16 w-full mx-auto bg-[#1313130D] mt-12 rounded-3xl">
+      <div className="w-[526px] flex flex-col gap-12">
+        <h1 className="text-[#131313] font-bold text-6xl">
+          Books to freshen up your bookshelf
         </h1>
-        <button className="btn-primary">Test</button>
+        <div>
+          <button className="btn-primary bg-[#23BE0A] text-white text-[20px] font-bold py-5 px-7 rounded-lg">
+            View The List
+          </button>
+        </div>
       </div>
-      <div>
-        <img className="w-3/12" src={bookImage} alt="" />
+      <div className="w-80 bg-[#1313130D]">
+        <img className="" src={bookImage} alt="" />
       </div>
     </div>
   );
